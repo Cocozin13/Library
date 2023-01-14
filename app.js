cf7ddee941b5f1eeb2a14fdd71cd6441b6318777
Cocozin13 <atumbananaxd2@gmail.com>
2023-01-14 00:01:19 +0000

    Missing Storage (Local + Cloud)

--- app.js
+++ app.js
@@ -1,52 +1,104 @@
+//Add Local Storage later + UI Improvements
+
+// Variables
 const booksConteiner = document.querySelector('.booksContainer')
 const newBookBtn = document.querySelector('.newBook')
 const submitNewBtn = document.querySelector('.submitNewBook')
 const bookForm = document.querySelector('.addNewBookForm')
+const bookFormContainer = document.querySelector('.formConteiner') 
 const bookTitle = document.querySelector('#title')
 const bookAuthor = document.querySelector('#author')
 const bookPages = document.querySelector('#pages')
 const bookRead = document.querySelector('#read')
 
-submitNewBtn.addEventListener("click", denySubmit)
-
 let books = []
+let book 
+
+//Buttons
+newBookBtn.addEventListener("click", () => {
+    bookFormContainer.style.display = 'block'
+    bookFormContainer.classList.toggle('show')
+})
+submitNewBtn.addEventListener("click", denySubmit)
+submitNewBtn.addEventListener("click", validateForm)
 
 //Constructor
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        return `${title} by ${author}; pages: ${pages}; ${read}`
+class Book {
+    constructor (title, author, pages, read) {
+        this.title = form.title.value
+        this.author = form.author.value
+        this.pages = form.pages.value
+        this.read = form.read.checked
+    }
+}
+
+// Renders library visual
+function render() {
+    const displays = document.querySelectorAll('.book')
+    displays.forEach(book => booksConteiner.removeChild(book))
+
+    for (let i = 0; i < books.length; i++) {
+        displayBooks(books[i])
     }
 }
 
 //Display books on the array
-function displayBooks() {
-    for (let i = 0; i < books.length; i++) {
-        let disBook = document.createElement('div')
-        disBook.textContent = books[i].title
-        booksConteiner.appendChild(disBook)
-    } 
+function displayBooks(item) {   
+
+        let bookDisplay = document.createElement('div')
+        let displayTitle = document.createElement('p')
+        let displayAuthor = document.createElement('p')
+        let displayPages = document.createElement('p')
+        let displayRead = document.createElement('button')
+        let deleteBtn = document.createElement('button')
+        
+        bookDisplay.classList.add('book')
+        deleteBtn.textContent = 'DELETE'
+        deleteBtn.classList.add('deleteBtn')
+        displayTitle.textContent = `"${item.title}"`
+        displayAuthor.textContent = `By ${item.author}`
+        displayPages.textContent = `${item.pages} pages`
+        
+        if (item.read === false) {
+            displayRead.textContent = "Not Read"
+            displayRead.style.backgroundColor = "red"
+        }else {
+            displayRead.textContent = "Read"
+            displayRead.style.backgroundColor = "green"
+        }
+         
+        bookDisplay.appendChild(displayTitle)
+        bookDisplay.appendChild(displayAuthor)
+        bookDisplay.appendChild(displayPages)
+        bookDisplay.appendChild(displayRead)
+        bookDisplay.appendChild(deleteBtn)
+        booksConteiner.appendChild(bookDisplay)
+
+        displayRead.addEventListener("click", () => {
+            item.read = !item.read
+            render()
+        })
+
+        deleteBtn.addEventListener("click", () => {
+            books.splice(books.indexOf(item),1)
+            render()
+        })
 }
 
 //Add books to array 
 function addBooks() {
-    const book = new Book(title, author, pages, read)
+    book = new Book(title, author, pages, read)
     books.push(book)
-    displayBooks()
+    render()
 }
 
 // Validate Form/Book values
 function validateForm() {
     if (bookTitle.value !== '' && bookAuthor.value !== '' && bookPages.value !== '' 
-    && bookPages.value > 0) {
-        if (checkbox.checked) {
-            addBooks(bookTitle.value, bookAuthor.value, bookPages.value, true)
-        } else {
-            addBooks(bookTitle.value, bookAuthor.value, bookPages.value, false)
-        }
+    && bookPages.value > 0) { 
+        addBooks(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value)
+        bookFormContainer.style.display = 'none'
+        bookFormContainer.classList.toggle('show')
     }
     bookForm.reset()
 }
@@ -55,5 +107,3 @@ function validateForm() {
 function denySubmit(event) {
     event.preventDefault()
 }
-
-addBooks()
